9d0597b5e6c66625c78ccaca7aca9ea85539eab4
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-27 14:00:32 +0200

    create database from express done

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = "3000";
-const db = require("./config/database.js")
+const db = require("./config/database.js");
 
-app.use(express.json())
+app.use(express.json());
 
+app.get("/createdbd", (req, res) => {
+  let sql = "CREATE DATABASE expressDB";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Database created :)...");
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Servidor levantado en el port 3000");
 });
-
