2e3d5eaa80a846742eccef61007abae7e89f9c31
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-27 13:39:44 +0200

    installed npms and folder config created

---
+++ config/database.js
@@ -0,0 +1,13 @@
+//* Le damos permisos para crear una database
+
+const mysql = require("mysql2")
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'admin123'
+})
+
+db.connect()
+
+module.exports = db;
